ac0d27d9229e4ed2229836a4a3efecd6cf7262ee
pranitjadhav123 <pranit812jadhav@gmail.com>
2021-07-11 21:08:58 +0530

    Bank1

---
+++ index.php
@@ -0,0 +1,59 @@
+<HTML>
+<head>
+	 <meta name="author" content="pranit" >
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+      <meta name="format-detection" content="telephone=no">
+      <link rel="stylesheet" href="style.css">
+      <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Caveat&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Caveat|Cookie&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Delius&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" crossorigin="anonymous">
+	<center><title>Banking System</title></center>
+	<link rel = "stylesheet" type = "text/css" href = "buttons.css">
+</head>
+
+<body style="background:linear-gradient(to bottom ,green,	#49ff33,	#49ff33,#49ff33	,#49ff33,green);background-attachment: fixed;background-repeat: no-repeat;background-size: cover;">
+	
+
+<link rel = "stylesheet" type = "text/css" href = "style.css">
+
+
+	<div class="one" style=" background-color:Crimson;
+   height: :50px;
+   width:100%;">
+		<table width="1330">
+            		
+			<table width="1200" align="center"  class = "t">
+                <tr>
+				<td style = "text-align:center"> <button class = "btn1"> Home </button></td> 	
+				<td style = "text-align:center"><a href="customer.php" target="frame"><button class = "btn2">View Customers</button></a></td>
+				<td style = "text-align:center"><a href="Transfermoney.php" target="frame"><button class = "btn2">Transfer Money</button></a></td>
+				<td style = "text-align:center"><a href="transactionhistory.php" target="frame"><button class = "btn2">Details of Transaction History</button></a></td>
+                </tr>
+            </table>
+        </table>
+	</div>
+	<div class="two">Welcome to </div>
+	<h1 >
+	BANK OF BOMBAY
+</h1>
+	<div class="three" >The Bank of Bombay Help User to Secure Transaction Between User .<br>Its Very easy process simple click on Transfer Money Button select Name of sender<br> Then select the user to whom you want to transfer money and Enter the amount that you want to transfer.<br>
+	<div class="four"> Now just click the submit button YOUR  TRANSACTION  IS  SUCCESSFUL.....)</div> 
+</div>
+
+<div class="five">Designed By PRANIT JADHAV<br>Contact Us:pranit812jadhav@gmail.com
+</div>
+
+      <span style="color: LightSalmon; padding: 200px;">
+
+          <br>        
+         <a style="margin-left: 600px;color:wheat;" href="https://twitter.com/pranit_bhai" onmouseover="this.style.color='#FFFF00'" onmouseout="this.style.color='blue'"><i class="fab fa-twitter fa-2x"></i></a>
+         <a style="margin-left: 10px;color:wheat;"href="https://github.com/pranitjadhav123" onmouseover="this.style.color='#FFFF00'" onmouseout="this.style.color='blue'" ><i class="fab fa-github fa-2x"></i></a>
+         <a style="margin-left:10px;color:wheat;"href="https://www.linkedin.com/in/pranit-jadhav-455780214" onmouseover="this.style.color='#FFFF00'" onmouseout="this.style.color='blue'"><i class="fab fa-linkedin fa-2x"></i></a>
+         
+         </span> 
+         </div>
+</body>
+</html>
